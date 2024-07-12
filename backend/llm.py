3129216dc2440565fc65f97368249eca005010db
Murilo <murilo@localhost.localdomain>
2024-07-12 09:11:37 -0300

    .

--- backend/llm.py
+++ backend/llm.py
@@ -8,34 +8,10 @@ import os
 
 embeddings = OpenAIEmbeddings()
 database_path = "faiss_index"
-history = []
-
-
-def update_history(history, user_message, ai_response):
-    # Adiciona a nova interação ao histórico
-    history.append(("human", user_message))
-    history.append(("ai", ai_response))
-
-    # Mantém apenas as últimas tres interações
-    if len(history) > 6:
-        history = history[-6:]
-
-    return history
-
-
-def format_history_for_prompt(history):
-    history_str = ""
-    for source, message in history:
-        if source == "human":
-            history_str += f"Usuário: {message}\n"
-        elif source == "ai":
-            history_str += f"Assistente: {message}\n"
-    return history_str
 
 
 def database_exists(database_path):
     return os.path.exists(database_path)
-
 
 def function_load_faiss_index():
     retriever = None
@@ -47,16 +23,11 @@ def function_load_faiss_index():
         retriever = faiss_index.as_retriever()
     return retriever
 
-
 # Function to get appropriate agent
-def get_agent_for_question(pergunta):
-    global history
-    formatted_history = format_history_for_prompt(history)
-    print(history, formatted_history)
-    response = agent_inicial.invoke(
-        {"history": formatted_history, "user_question": pergunta}
-    )
-    response = response["answer"]
+def get_agent_for_question(pergunta, history):
+    print(history)
+    response = agent_inicial.invoke({"history": history, "user_question": pergunta})
+    response = response['answer']
     print(response)
     if "arquivo" in response:
         return 1
@@ -68,25 +39,20 @@ def get_agent_for_question(pergunta):
         return 4
     else:
         return 0
-
-
-def chain_invoke(pergunta, docs):
-    global history
-    formatted_history = format_history_for_prompt(history)
+        
+def chain_invoke(pergunta, docs, history):
     retriever = function_load_faiss_index()
-    escolha = get_agent_for_question(pergunta)
-
+    escolha = get_agent_for_question(pergunta, history)
+    
     if escolha == 0:  # sem prompt especial
-        answer = normal_agente.invoke(
-            {"user_question": pergunta, "history": formatted_history}
-        )
-        answer = answer["answer"]
-
+        answer = normal_agente.invoke({"user_question": pergunta, "history": history})
+        answer = answer['answer']
+    
     elif escolha == 1:  # prompt arquivo
         if database_exists(database_path):
             answer = agent_consulta_vetorial.invoke({"user_question": pergunta})
             # Consulta ao banco vetorial
-            answer = answer["answer"]
+            answer = answer['answer']
             relevant_docs = retriever.invoke(answer)
             context = "\n".join([doc.page_content for doc in relevant_docs])
             if len(context) > 30000:
@@ -95,22 +61,16 @@ def chain_invoke(pergunta, docs):
         else:
             context = "Ainda não tem arquivos vetorizados avise o usuario."
         print(context)
-        answer = agent_arquivo.invoke(
-            {"user_question": pergunta, "docs": context, "history": formatted_history}
-        )
-        answer = answer["answer"]
-
+        answer = agent_arquivo.invoke({"user_question": pergunta, "docs": context, "history": history})
+        answer = answer['answer']
+        
     elif escolha == 2:  # prompt fluxograma
-        answer_inicial = agent_analise_fluxo.invoke(
-            {"code": docs, "user_question": pergunta}
-        )
-        answer_inicial = answer_inicial["answer"]
-        answer = agent_fluxograma.invoke(
-            {"flow_description": answer_inicial, "user_question": pergunta}
-        )
-        answer = answer["answer"]
+        answer_inicial = agent_analise_fluxo.invoke({"code": docs, "user_question": pergunta})
+        answer_inicial = answer_inicial['answer']
+        answer = agent_fluxograma.invoke({"flow_description": answer_inicial, "user_question": pergunta})
+        answer = answer['answer']
         answer = formata_resposta.invoke({"user_question": answer})
-        answer = answer["answer"]
+        answer = answer['answer']
         nome_fluxograma = "fluxograma" + datetime.now().strftime("%Y%m%d_%H%M%S")
         try:
             criar_fluxograma(nome_fluxograma, answer)
@@ -121,25 +81,21 @@ def chain_invoke(pergunta, docs):
         answer = f"\n\nClique <a href='{caminho_pdf}' download='{nome_fluxograma_com_extensao}'> aqui </a> para baixar o fluxograma"
         answer_inicial = "**Definição do fluxograma**: \n\n" + answer_inicial + answer
         answer = answer_inicial
-
+    
     elif escolha == 3:  # prompt regras
-        answer_regras = agent_regras_negocios.invoke(
-            {"code": docs, "history": formatted_history, "user_question": pergunta}
-        )
-        answer_regras = answer_regras["answer"]
+        answer_regras = agent_regras_negocios.invoke({"code": docs, "history": history, "user_question": pergunta})
+        answer_regras = answer_regras['answer']
         answer = answer_regras
-
+        
         # Salvar a resposta em regras.txt
-        with open("regras.txt", "w", encoding="utf-8") as file:
+        with open("regras.txt", "w", encoding='utf-8') as file:
             file.write(answer)
-
+    
     elif escolha == 4:  # prompt analise codigo
-        answer = alteracao_agent.invoke({"user_question": pergunta, "code": docs})
-        answer = answer["answer"]
-
-    try:
-        history = update_history(history, pergunta, answer)
-    except Exception as e:
-        history = []
+        answer = alteracao_agent.invoke({
+            "user_question": pergunta,
+            "code": docs
+        })
+        answer = answer['answer']
 
     return answer
